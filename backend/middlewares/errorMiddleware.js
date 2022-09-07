f34a24060302d5e70fceb5260bc89a61631e160d
bg <gondekbogna69@gmail.com>
2022-09-07 11:07:53 +0100

    repaired middleware init

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -8,6 +8,7 @@ const notFound = (req, res, next) => {
 
 // converting thrown error into a structured form
 const errorHandler = (err, req, res, next) => {
+  console.log(err);
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
   res.json({

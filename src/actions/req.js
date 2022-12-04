112a9e03b45475979ee8d1d26f3384d9bae54689
bg <gondekbogna69@gmail.com>
2022-12-04 14:40:45 +0000

    visible email in scratch-gui url

--- src/actions/req.js
+++ src/actions/req.js
@@ -5,9 +5,9 @@ export const fetchLastTask = (setMapLevels) => async (dispatch) => {
   try {
     const userEmail = JSON.parse(localStorage.getItem("userInfo")).email;
     const response = await axios.get("/users/task/" + userEmail);
-    let salt = await bcrypt.genSalt(10);
-    let emailHashed = await bcrypt.hash(userEmail, salt);
-    dispatch(setMapLevels({ email: emailHashed, index: response.data }));
+    //let salt = await bcrypt.genSalt(10);
+    //let emailHashed = await bcrypt.hash(userEmail, salt);
+    dispatch(setMapLevels({ email: userEmail, index: response.data }));
   } catch (error) {
     //console.log(error);
   }

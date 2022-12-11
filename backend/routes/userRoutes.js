6434bf49b514cc57a8a8d622fa628a96e67d5fb3
bg <gondekbogna69@gmail.com>
2022-12-11 23:19:15 +0000

    someone must have changed route by accident

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -23,7 +23,7 @@ const userRouter = require("express").Router();
 
 userRouter.get(userPrefix + "/:email", getStudentsOfTeacher);
 userRouter.get(userPrefix + "/homework/:email", getHomework);
-userRouter.get(userPrefix + "/api/task/:email", getLastTaskDone);
+userRouter.get(userPrefix + "/task/:email", getLastTaskDone);
 userRouter.delete(userPrefix + "/:email", removeUserRequest);
 userRouter.put(userPrefix + "/addToGroup", addStudentToGroup);
 userRouter.put(userPrefix + "/update", updateUsersVarRequest);
